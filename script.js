6544de93591a1d4840d0073e36fb44da9ae2ab4a
vic749 <eduardovictor749@gmail.com>
2026-05-14 08:45:35 -0300

    add personal links

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-me-light.png")
   } else {
     img.setAttribute("src", "./assets/avatar-me.png")
   }
